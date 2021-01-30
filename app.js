7ea1255d2de3186399535875b3f16fc392198ce3
Cris <cristopher.26hp@gmail.com>
2021-01-29 21:37:08 -0600

    Deploy en heroku

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-    res.end('Hola Mundo');
-});
+const express = require('express');
+const app = express();
 
 const puerto = process.env.PORT || 3000;
-server.listen(puerto, () => {
-    console.log('Escuchando solicitudes');
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/index', (req, res) => {
+    res.render('index.html');
+});
+
+app.get('/formulario', (req, res) => {
+    res.render('formulario.html');
+});
+
+app.listen(puerto, () => {
+    console.log('Servidor a su servicio');
 });
