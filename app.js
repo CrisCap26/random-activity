bc410cb7964336be486ef59dc210fc6574e024b7
Cris <cristopher.26hp@gmail.com>
2021-02-12 17:09:34 -0600

    Esta es la buena

--- app.js
+++ app.js
@@ -16,13 +16,13 @@ const puerto = process.env.PORT || 3000;
 
 //Conexión a base de datos
 const mongoose = require('mongoose');
-/*
+
 const user = 'Admin';
 const password = '2vPXyWv49Ivs6ke2';
 const dbname = 'Registro';
-*/
-const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.x7dcl.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
 
+//const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.x7dcl.mongodb.net/${process.env.DBNAME}?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${user}:${password}@cluster0.x7dcl.mongodb.net/${dbname}?retryWrites=true&w=majority`;
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
 
     .then(()=> console.log('Base de datos conectada'))
